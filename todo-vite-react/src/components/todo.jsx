def17c936000cb7698d947c9db868e74defff113
Mustafa Göleç <mustafagolec36@gmail.com>
2024-05-02 14:44:13 +0300

    Add, Delete and Local Storage
    Now you can add and delete new tasks. Also all the tasks are now being stored in local storage.

--- todo-vite-react/src/components/todo.jsx
+++ todo-vite-react/src/components/todo.jsx
@@ -1,8 +1,15 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function Todo() {
     const [task, setTask] = useState('')
     const [tasks,setTasks] = useState([])
+
+    useEffect(()=>{
+        if(localStorage.getItem("localTasks")){
+            const storedList = JSON.parse(localStorage.getItem("localTasks"))
+            setTasks(storedList)
+        }
+    })
 
     const addTask = (e)=>{
         if(task){
@@ -12,6 +19,20 @@ export default function Todo() {
             setTask("")
         }
     }
+
+    const handleDelete = (task) => {
+        const deleted = tasks.filter((t) => t.id !== task.id)
+        setTasks(deleted)
+        localStorage.setItem("localTasks", JSON.stringify(deleted))
+    }
+
+    const handleClear=()=>{
+        setTasks([])
+        localStorage.removeItem("localTasks")
+    }
+
+
+
   return (
     <div className='container row'>
         <h1 className="mt-3 text-white">To-Do App</h1>
@@ -30,6 +51,26 @@ export default function Todo() {
                 : null
             }
         </div>
+        {tasks.map((task)=>(
+            <React.Fragment key = {task.id}>
+                <div className="col-11">
+                    <span className="form-control bg-white btn mt-2" style={{textAlign: "left", fontWeight:"bold"}}>
+                        {task.title}
+                    </span>
+                </div>
+
+                <div className="col-1">
+                    <button className="mt-2 btn btn-warning material-icons" onClick={()=>handleDelete(task)}>delete</button>
+                </div>
+            </React.Fragment>
+        ))}
+        {!tasks.length ? null:(
+            <div>
+                <button className="btn btn-secondary mt-4 mb-4" onClick={() => handleClear()}>
+                    Clear
+                </button>
+            </div>
+        )}
     </div>
   )
 }
